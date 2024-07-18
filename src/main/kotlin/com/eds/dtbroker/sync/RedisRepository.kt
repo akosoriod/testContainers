a2a411b85ab9322e8d68a11b4717f0f4cf3905fa
Luis Guerreiro <luisguerreiro@lithia.com>
2024-07-18 19:12:52 -0300

    Adding spring repositories skeleton

---
+++ src/main/kotlin/com/eds/dtbroker/sync/RedisRepository.kt
@@ -0,0 +1,16 @@
+package com.eds.dtbroker.sync
+
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.stereotype.Repository
+
+@Repository
+class RedisRepository(
+    val redisTemplate: StringRedisTemplate,
+){
+
+    // Adds a list of JSON records to REDIS 
+    fun addRecordsToCache(List<String>){
+        //TODO implement redis operations logic here
+        redisTemplate.opsForValue()
+    }
+}
---
+++ src/main/kotlin/com/eds/dtbroker/sync/SnowflakeRepository.kt
@@ -0,0 +1,22 @@
+package com.eds.dtbroker.sync
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Repository
+
+@Repository
+class SnowflakeRepository(
+    val jdbcTemplate: NamedParameterJdbcTemplate,
+) {
+    // Get all records from a sync table
+    fun getAllRecords(schema: String, table: String): List<String> {
+        // TODO implement query logic
+        // val sql = "select * from $schema.$table;"
+
+        // val records = jdbcTemplate.query(sql) { row, _ ->
+        //     // unpack columns
+        //     // convert to JSON
+        // }
+
+         return listOf(<String>)
+    }
+}
